61d0031db3668a09fa22efe946248a509c9441c7
Alexander <67597509+AlexanderK-123@users.noreply.github.com>
2021-10-02 14:48:24 +0300

    Add files via upload

--- epam_Kuharchik/src/by/epam/tr/homework/lesson1/Task10.java
+++ epam_Kuharchik/src/by/epam/tr/homework/lesson1/Task10.java
@@ -6,22 +6,30 @@ public class Task10 {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.println("Enter n:");
-		int n = in.nextInt();
+		System.out.println("Enter even n:");
+		int n = 0;
+		do {
+			n = in.nextInt();	
+		}while(n % 2 != 0 || n == 0);
+		in.close();
+		
 		int[][] a = new int[n][n];
+		int k = n;
 		for(int i = 0; i < n; i++) {
-			if(i % 2 != 0) {
+			if(i == 0 || i % 2 == 0) {
 				for(int j = 0; j < n; j++) {
 					a[i][j] = ++j;
-				}
+					j--;
+				} 
 			}else {
-				for(int j = n; j > 0; j--) {
-					a[i][j--] = n;
+				k = n;
+				for(int j = 0; j < n ; j++) {
+					a[i][j] = k--;
 				}
-			}	
+			}
 		}
 		
-		for(int i = 0;i < n;i++) {
+		for(int i = 0; i < n; i++) {
 			System.out.println();
 			for(int j = 0; j < n; j++) {
 				System.out.print(a[i][j] + " ");

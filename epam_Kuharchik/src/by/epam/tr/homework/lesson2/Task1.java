4398c9b8a90e0ec6bc4c23c8c6a53f80229764c6
Alexander <67597509+AlexanderK-123@users.noreply.github.com>
2021-10-06 07:29:41 +0300

    Add files via upload

--- epam_Kuharchik/src/by/epam/tr/homework/lesson2/Task1.java
+++ epam_Kuharchik/src/by/epam/tr/homework/lesson2/Task1.java
@@ -2,36 +2,43 @@ package by.epam.tr.homework.lesson2;
 
 import java.util.Scanner;
 
+/**
+ * —оставить линейную программу, печатающую значение true, если указанное
+ * высказывание €вл€етс€ истинным, и false Ч в противном случае: —умма двух
+ * первых цифр заданного четырехзначного числа равна сумме двух его последних
+ * цифр.
+ */
+
 public class Task1 {
-	
+
 	public static boolean calculation(int n) {
-		if(String.valueOf(Math.abs(n)).length() == 4 && (n%10+(n/10)%10) == (n/1000 + (n/100)%10)) {
+		if (String.valueOf(Math.abs(n)).length() == 4 && (n % 10 + (n / 10) % 10) == (n / 1000 + (n / 100) % 10)) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
 	}
-	
+
 	public static boolean validNumber(int n) {
-		if(String.valueOf(n).length() == 4 && n > 0) {
-				return true;
-		}else {
+		if (String.valueOf(n).length() == 4 && n > 0) {
+			return true;
+		} else {
 			System.out.println("Incorrect number! Try again:");
 			return false;
-		} 
+		}
 	}
-	
+
 	public static int inputNumber() {
 		Scanner in = new Scanner(System.in);
 		int number = 0;
 		do {
 			System.out.println("Enter number:");
 			number = in.nextInt();
-		}while(!validNumber(number));
+		} while (!validNumber(number));
 		in.close();
 		return number;
 	}
-	
+
 	public static void main(String[] args) {
 		System.out.println(calculation(inputNumber()));
 	}

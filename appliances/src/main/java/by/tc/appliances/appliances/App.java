e497e2e7030fe341c3944065a0c9f2ed2c3a5905
Alexander <67597509+AlexanderK-123@users.noreply.github.com>
2021-11-02 07:57:03 +0300

    Add files via upload

---
+++ appliances/src/main/java/by/tc/appliances/appliances/App.java
@@ -0,0 +1,13 @@
+package by.tc.appliances.appliances;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
